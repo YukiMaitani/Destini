63f482e3f98a94b9ad17de360e20067d69675cfb
YukiMaitani <fightzoi1997@gmail.com>
2022-06-15 03:17:38 +0900

    課題クリア

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setButton()
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.tag)
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    func setButton(){
+        choice1Button.tag = 1
+        choice2Button.tag = 2
+    }
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
-import Foundation
+import Metal
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice1Destination: Int
+    var choice2: String
+    var choice2Destination: Int
+}
